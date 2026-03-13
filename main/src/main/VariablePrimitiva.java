2c41b1e36f7d1969a211cfdf14973ad6af14e246
Jesy05 <jgonzalezj@uamv.edu.ni>
2026-03-13 16:09:30 -0600

    3RA CLASE S1

---
+++ main/src/main/VariablePrimitiva.java
@@ -0,0 +1,22 @@
+package main;
+
+public class VariablePrimitiva {
+    public static void main(String[] args) {
+        int numero = 10;
+        System.out.println("El número es: " + numero);
+
+        float capital = 1999.99f;
+        System.out.println("El capital es: " + capital);
+
+        double decimal = 19999999.99;
+        System.out.println("El decimal es: " + decimal);
+
+        boolean estado = true;
+        System.out.println("El estado es: " + estado);
+
+        String nombre = "Dominick";
+        System.out.println("El nombre es: " + nombre);
+
+
+    }
+}
---
+++ main/src/main/VariableReferencia.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class VariableReferencia {
+
+    public static void main(String[] args) {
+        //Scanner leer = new Scanner(System.in);
+        //System.out.println("Dime tu nombre: ");
+        //String nombre= leer.nextLine();
+        String nombre = JOptionPane.showInputDialog("Dime tu nombre: ");
+        String mensaje = "Hola " + nombre;
+
+        JOptionPane.showMessageDialog(null,  mensaje+"Bienvenido a UAM");
+
+
+
+
+
+    }
+}
