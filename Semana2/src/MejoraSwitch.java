97dc0061c3dad0e9168357c48de6e962682f3488
alee1616 <maguillenc@uamv.edu.ni>
2026-03-16 16:19:37 -0600

    Ejercicio2

--- Semana2/src/MejoraSwitch.java
+++ Semana2/src/MejoraSwitch.java
@@ -1,13 +1,17 @@
 public class MejoraSwitch {
     public static void main(String[] args) {
+        // se define una variable de prueba para evaluar el día.
         int dia = 3;
 
+        // uso de el operador de flecha (->) para asignar el resultado de forma directa
         System.out.println(switch (dia) {
             case 1 -> "Lunes";
             case 2 -> "Martes";
             case 3 -> "Miércoles";
             case 4 -> "Jueves";
             case 5 -> "Viernes";
+
+            // un 'default' obligatorio aquí para asegurar que siempre se devuelva un String
             default -> "Fin de semana u otro día";
         });
     }
